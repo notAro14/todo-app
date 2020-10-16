9de4f582e9c0e2273c4e10b3cc8765dc6e70e8c6
Aro Andriamaro <ni.aro.aina@gmail.com>
2020-10-17 01:32:30 +0200

    Updates

---
+++ precache-manifest.b3cc2a8dc6c49608e7cc2caf2a58142f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edc2b873bc199b7f6cc863b02a536a9",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "da0aef82139599c4f1fa",
+    "url": "/todo-app/static/js/2.468cd3c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app/static/js/2.468cd3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb12855ece0a64fac79",
+    "url": "/todo-app/static/js/main.32851459.chunk.js"
+  },
+  {
+    "revision": "b490eda1f5bb57da1ff2",
+    "url": "/todo-app/static/js/runtime-main.22d29886.js"
+  }
+]);
